375a2090215da0ee4aaf614b0b5dbbfd8fdd1b9a
Muhammad Usama <37137366+MUsamaRaza@users.noreply.github.com>
2018-11-29 11:59:46 +0500

    Create App.js

---
+++ Face_Detect/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import StartPage from "./components/StartPage";
+import Questions from "./components/Questions";
+import Finish from "./components/Finish";
+import Quiz from './Quiz';
+// import MyNavigator from './MyNavigator';
+
+const App = createStackNavigator({
+  StartPage: { screen: StartPage },
+  Questions: { screen: Questions },
+  Finish: { screen: Finish }
+});
+
+export default App;
